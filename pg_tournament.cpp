e2d09d4972762c9881d72907554369df38fa3a3b
octopusflyz <octopusfly@stu.pku.edu.cn>
2024-05-23 09:44:12 +0800

    update 0523

--- pg_tournament.cpp
+++ pg_tournament.cpp
@@ -1,11 +1,20 @@
 #include "pg_tournament.h"
 #include "ui_pg_tournament.h"
+#include<QTimer>
+#include<QDebug>
+#include<QPainter>
 
 pg_tournament::pg_tournament(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::pg_tournament)
 {
     ui->setupUi(this);
+}
+void pg_tournament::paintEvent(QPaintEvent *event){
+    //qDebug()<<flag;
+    QPainter painter(this);
+    QRect rec3(24,60,349,300);
+    painter.drawPixmap(rec3,QPixmap(":/image/tournament.png"));
 }
 
 pg_tournament::~pg_tournament()
--- pg_twoplayers.h
+++ pg_twoplayers.h
@@ -14,6 +14,14 @@ class pg_twoplayers : public QWidget
 public:
     explicit pg_twoplayers(QWidget *parent = nullptr);
     ~pg_twoplayers();
+    void paintEvent(QPaintEvent *event);
+    //小人
+    int posx1=50,posy1=260;
+    int posx2=575,posy2=260;
+    //金币
+    int px1=130,py1=300;
+    int px2=565,py2=300;
+    bool flag=0;//投币前
 
 private:
     Ui::pg_twoplayers *ui;

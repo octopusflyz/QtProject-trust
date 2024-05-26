165ce1534cec9ec0a1e16833d54085e81611e676
octopusflyz <octopusfly@stu.pku.edu.cn>
2024-05-27 00:44:58 +0800

    chioces added in pg_allplayers

---
+++ slider.cpp
@@ -0,0 +1,17 @@
+#include "slider.h"
+#include "ui_slider.h"
+
+slider::slider(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::slider)
+{
+    ui->setupUi(this);
+    //qspinbox 移动 qslider移动 关联
+    connect(ui->spinBox,&QSpinBox::valueChanged,ui->horizontalSlider,&QSlider::setValue);
+    connect(ui->horizontalSlider,&QSlider::valueChanged,ui->spinBox,&QSpinBox::setValue);
+}
+
+slider::~slider()
+{
+    delete ui;
+}
